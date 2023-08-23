8db26259ea571fafec25facdf01a3d02ff164df3
rishikeshydv <rishikeshadh4@gmail.com>
2023-08-23 02:38:23 -0400

    chore: components added

--- nextjs/tailwind.config.ts
+++ nextjs/tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      textColor: {
+        'beige':'#F7934C'
+      },
+      backgroundColor: {
+        'beige':'#F7934C'
+      },
     },
   },
   plugins: [],
